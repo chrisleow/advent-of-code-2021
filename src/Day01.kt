1693dc22de9fbbfd8c61a22e0a18fd8f8eb38780
Chris Leow <chris@complyadvantage.com>
2021-12-01 08:27:50 +0000

    Successful solution to day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val readings = input.map { it.toInt() }
+        val deltas = readings
+            .zip(readings.drop(1))
+            .map { (a, b) -> b - a }
+        return deltas.count { it > 0 }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val readings = input.map { it.toInt() }
+        val windows = (0 .. readings.size - 3)
+            .map { readings.slice(it until it + 3).sum() }
+        val deltas = windows
+            .zip(windows.drop(1))
+            .map { (a, b)-> b - a }
+        return deltas.count { it > 0 }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
