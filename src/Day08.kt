61470f0ed17760af1f0b8f10e39482dd7921e885
Chris Leow <chris@complyadvantage.com>
2021-12-08 12:40:06 +0000

    Represent patterns as sets not strings

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,16 +1,16 @@
 fun main() {
 
     val digits = mapOf(
-        "ABCEFG" to 0,
-        "CF" to 1,
-        "ACDEG" to 2,
-        "ACDFG" to 3,
-        "BCDF" to 4,
-        "ABDFG" to 5,
-        "ABDEFG" to 6,
-        "ACF" to 7,
-        "ABCDEFG" to 8,
-        "ABCDFG" to 9,
+        "ABCEFG".toSet() to 0,
+        "CF".toSet() to 1,
+        "ACDEG".toSet() to 2,
+        "ACDFG".toSet() to 3,
+        "BCDF".toSet() to 4,
+        "ABDFG".toSet() to 5,
+        "ABDEFG".toSet() to 6,
+        "ACF".toSet() to 7,
+        "ABCDEFG".toSet() to 8,
+        "ABCDFG".toSet() to 9,
     )
 
     data class InputLine(val signalPatterns: List<String>, val outputPatterns: List<String>)
@@ -25,42 +25,40 @@ fun main() {
 
     fun decodeOutput(line: InputLine): Int {
         val scrambledPatterns = line.signalPatterns + line.outputPatterns
-        val digitPatternsByLength = digits.keys.groupBy { it.length }
+        val digitPatternsByLength = digits.keys.groupBy { it.size }
 
-        // boil down set of "possibilities" assuming a digit pattern to an output pattern
-        fun Map<Char, Set<Char>>.restrict(scrambled: String, real: String): Map<Char, Set<Char>> {
-            assert(scrambled.length == real.length) {
+        // boil down set of possibilities assuming a digit pattern to an output pattern
+        fun Map<Char, Set<Char>>.restrict(scrambled: Set<Char>, real: Set<Char>): Map<Char, Set<Char>> {
+            assert(scrambled.size == real.size) {
                 "restricted 'scrambled' and 'real' strings are the same size"
             }
             return this.mapValues { (char, possibleChars) ->
                 when (char in scrambled) {
-                    true -> possibleChars intersect real.toSet()
-                    false -> possibleChars - real.toSet()
+                    true -> possibleChars intersect real
+                    false -> possibleChars - real
                 }
             }
         }
 
-        // search by testing hypotheses for each digit (and restricting the map as we go)
+        // search recursively by testing hypotheses for each digit (and restricting the map as we go)
         fun search(mapping: Map<Char, Set<Char>>, index: Int): Map<Char, Char>? = when {
             mapping.any { it.value.isEmpty() } -> null
             index >= scrambledPatterns.size -> mapping.mapValues { it.value.first() }
             else -> {
-                val scrambledPattern = scrambledPatterns[index]
-                digitPatternsByLength[scrambledPattern.length]?.firstNotNullOfOrNull {
+                val scrambledPattern = scrambledPatterns[index].toSet()
+                digitPatternsByLength[scrambledPattern.size]?.firstNotNullOfOrNull {
                     search(mapping.restrict(scrambledPattern, it), index + 1)
                 }
             }
         }
 
+        // start search at the beginning with all possibilities
         val mapping = search("abcdefg".associateWith { "ABCDEFG".toSet() }, 0)
             ?: error("Should have a final mapping")
 
         return line.outputPatterns
             .map { pattern ->
-                val realPattern = pattern
-                    .mapNotNull { mapping[it] }
-                    .sorted()
-                    .joinToString("")
+                val realPattern = pattern.mapNotNull { mapping[it] }.toSet()
                 digits[realPattern] ?: error("Should have a digit pattern for: $realPattern")
             }
             .fold(0) { acc, d -> (acc * 10) + d }
@@ -69,7 +67,7 @@ fun main() {
     fun part1(input: List<String>): Int {
         val inputLines = parseInput(input)
         val reverseDigits = digits.asSequence().associate { Pair(it.value, it.key) }
-        val lengths = listOf(1, 4, 7, 8).map { reverseDigits[it]?.length ?: 0 }
+        val lengths = listOf(1, 4, 7, 8).map { reverseDigits[it]?.size ?: 0 }
         return inputLines
             .flatMap { it.outputPatterns }
             .count { it.length in lengths }
