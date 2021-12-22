44fbfde3f49f54efb144441579dbdfb6147c7b17
Chris Leow <chris@complyadvantage.com>
2021-12-22 11:14:58 +0000

    Less insane branching with 'anti-cuboids'

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,4 +1,5 @@
 data class Cuboid(val xRange: IntRange, val yRange: IntRange, val zRange: IntRange) {
+    fun isEmpty() = (xRange.isEmpty() || yRange.isEmpty() || zRange.isEmpty())
     val volume = listOf(xRange, yRange, zRange).fold(1L) { product, range ->
         if (range.isEmpty()) 0L else product * (range.last - range.first + 1)
     }
@@ -9,6 +10,8 @@ sealed class Instruction {
     data class On(override val cuboid: Cuboid) : Instruction()
     data class Off(override val cuboid: Cuboid) : Instruction()
 }
+
+data class State(val cuboids: List<Cuboid>, val antiCuboids: List<Cuboid>)
 
 fun main() {
 
@@ -26,88 +29,37 @@ fun main() {
             }
     }
 
-    // partition a range into (left-remainder, intersection, right-remainder), one of these may be empty
-    // need to deal with several cases here ...
-    fun IntRange.partitionBy(other: IntRange): Pair<IntRange, List<IntRange>> {
-        return when {
-            other.first in this && other.last in this -> {
-                Pair((other.first .. other.last),
-                    listOf((this.first until other.first), (other.last + 1 .. this.last)))
+    fun IntRange.intersect(other: IntRange) =
+        (maxOf(this.first, other.first) .. minOf(this.last, other.last))
+
+    fun Cuboid.intersect(other: Cuboid) = Cuboid(
+        xRange = this.xRange.intersect(other.xRange),
+        yRange = this.yRange.intersect(other.yRange),
+        zRange = this.zRange.intersect(other.zRange),
+    )
+
+    fun State.apply(instruction: Instruction): State {
+        return when (instruction) {
+            is Instruction.On -> {
+                val newCuboids = this.antiCuboids
+                    .map { it.intersect(instruction.cuboid) }
+                    .filter { !it.isEmpty() }
+                val newAntiCuboids = this.cuboids
+                    .map { it.intersect(instruction.cuboid) }
+                    .filter { !it.isEmpty() }
+                State(cuboids + newCuboids + instruction.cuboid, antiCuboids + newAntiCuboids)
             }
-            other.first in this -> {
-                Pair((other.first .. this.last), listOf(this.first until other.first))
-            }
-            other.last in this -> {
-                Pair((this.first .. other.last), listOf(other.last + 1 .. this.last))
-            }
-            other.first < this.first && other.last > this.last -> {
-                Pair(this, emptyList())
-            }
-            else -> {
-                Pair(IntRange.EMPTY, listOf(this))
+            is Instruction.Off -> {
+                val newAntiCuboids = this.cuboids
+                    .map { it.intersect(instruction.cuboid) }
+                    .filter { !it.isEmpty() }
+                val newCuboids = this.antiCuboids
+                    .map { it.intersect(instruction.cuboid) }
+                    .filter { !it.isEmpty() }
+                State(cuboids + newCuboids, antiCuboids + newAntiCuboids)
             }
         }
     }
-
-    // removes any intersection with given cuboid
-    fun Cuboid.remove(other: Cuboid): List<Cuboid> {
-        val (xIntersect, xRemainders) = this.xRange.partitionBy(other.xRange)
-        val (yIntersect, yRemainders) = this.yRange.partitionBy(other.yRange)
-        val (zIntersect, zRemainders) = this.zRange.partitionBy(other.zRange)
-
-        // give back all cuboids except the intersection cuboid
-        val intersectCuboid = Cuboid(xIntersect, yIntersect, zIntersect)
-        return (xRemainders + listOf(xIntersect)).flatMap { xRange ->
-            (yRemainders + listOf(yIntersect)).flatMap { yRange ->
-                (zRemainders + listOf(zIntersect))
-                    .map { zRange -> Cuboid(xRange, yRange, zRange) }
-                    .filter { it != intersectCuboid && it.volume > 0 }
-            }
-        }
-    }
-
-    fun List<IntRange>.reduce(): List<IntRange> {
-        if (this.isEmpty()) {
-            return emptyList()
-        }
-
-        val sortedRanges = this.sortedBy { it.first }
-        val initialRange = sortedRanges.first()
-        val initialState: Triple<List<IntRange>, Int, Int> = Triple(emptyList(), initialRange.first, initialRange.last)
-        val (ranges, first, last) = sortedRanges
-            .drop(1)
-            .fold(initialState) { (ranges, first, last), range ->
-                if (last + 1 == range.first) {
-                    Triple(ranges, first, range.last)
-                } else {
-                    Triple(ranges + listOf(first .. last), range.first, range.last)
-                }
-            }
-        return ranges + listOf(first .. last)
-    }
-
-    // merge adjacent cuboids where possible
-    fun List<Cuboid>.reduce() = this
-        .groupBy { cuboid -> Pair(cuboid.xRange, cuboid.yRange) }
-        .flatMap { (xyRanges, cuboids) ->
-            val (xRange, yRange) = xyRanges
-            cuboids.map { it.zRange }.reduce().map { zRange -> Cuboid(xRange, yRange, zRange) }
-        }
-//        .groupBy { cuboid -> Pair(cuboid.xRange, cuboid.zRange) }
-//        .flatMap { (xzRanges, cuboids) ->
-//            val (xRange, zRange) = xzRanges
-//            cuboids.map { it.yRange }.reduce().map { yRange -> Cuboid(xRange, yRange, zRange) }
-//        }
-//        .groupBy { cuboid -> Pair(cuboid.yRange, cuboid.zRange) }
-//        .flatMap { (yzRanges, cuboids) ->
-//            val (yRange, zRange) = yzRanges
-//            cuboids.map { it.xRange }.reduce().map { xRange -> Cuboid(xRange, yRange, zRange) }
-//        }
-
-    fun List<Cuboid>.apply(instruction: Instruction): List<Cuboid> = this
-        .flatMap { cuboid -> cuboid.remove(instruction.cuboid) }
-        .plus(if (instruction is Instruction.On) listOf(instruction.cuboid) else emptyList())
-        .reduce()
 
     fun part1(input: List<String>): Long {
         val instructions = parseInput(input)
@@ -116,16 +68,16 @@ fun main() {
                     it.first >= -50 && it.last <= 50
                 }
             }
-        return instructions
-            .fold(emptyList<Cuboid>()) { cuboids, instruction -> cuboids.apply(instruction) }
-            .sumOf { cuboid -> cuboid.volume }
+        val initialState = State(emptyList(), emptyList())
+        val finalState = instructions.fold(initialState) { state, instruction -> state.apply(instruction) }
+        return finalState.cuboids.sumOf { it.volume } - finalState.antiCuboids.sumOf { it.volume }
     }
 
     fun part2(input: List<String>): Long {
         val instructions = parseInput(input)
-        return instructions
-            .fold(emptyList<Cuboid>()) { cuboids, instruction -> cuboids.apply(instruction) }
-            .sumOf { cuboid -> cuboid.volume }
+        val initialState = State(emptyList(), emptyList())
+        val finalState = instructions.fold(initialState) { state, instruction -> state.apply(instruction) }
+        return finalState.cuboids.sumOf { it.volume } - finalState.antiCuboids.sumOf { it.volume }
     }
 
     // test
